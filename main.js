e41277bd1bc1da918c9a2333ff0f4bf0585f1dbe
wmckeever <wil.mckeever@gmail.com>
2022-11-20 01:32:04 -0500

    Added search button functionality and console.log tests. At this stage the code seems to be working but is not returning the values from spoonacular.

--- main.js
+++ main.js
@@ -9,8 +9,18 @@
 
 
 
+//*<------------- gloabal variables ------------->*//
+//*<----------------------------------------->*//
+let myAPIKey = "?apiKey=63aa00c3b9a14b4d81e35606c1528c2c";
+let searchUrl = "https://api.spoonacular.com/recipes/findByIngredients";
+
+
 //*<------------- querySelectors ------------->*//
 //*<----------------------------------------->*//
+let userInput = document.querySelector('#userInput');
+let searchButton = document.querySelector('#searchButton')
+
+
 
 
 
@@ -18,33 +28,18 @@
 //*<------------- API Fetch Call ------------->*//
 //*<----------------------------------------->*//
 
-  fetch('https://api.spoonacular.com/recipes/findByIngredients?apiKey=63aa00c3b9a14b4d81e35606c1528c2c')
+searchButton.addEventListener('click', (event) => {
+    console.log("Search Button Works")
+    console.log("User Iput is working and prints out:")
+    console.log(userInput.value);  
+  event.preventDefault();
+
+  fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=63aa00c3b9a14b4d81e35606c1528c2c&${userInput}`)
     .then(function(response){
-        console.log("json request is working")
           return response.json()
     })
     .then(function(data){
       console.log(data);
-       
-    });
+           })
 
-
-
-
-
-
-
-
-
-
-
-
-// let mealPlanAPIrequest = async () => {
-//   let response = await fetch('https://api.spoonacular.com/mealplanner/generate -- https://api.spoonacular.com/mealplanner/generate?apiKey=YOUR-API-KEY=63aa00c3b9a14b4d81e35606c1528c2ctimeFrame=day');
-  
-//   let data = await response.json();
-//   console.log(data);
-//   //img.src = data.message
-
-//   console.log(data);
-//   };
+          });
